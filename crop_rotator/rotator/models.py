8d762d7077f4dd72b1e597156024a9217f5f5f7a
Jakub <j.kozdrowicz@apdeveloper.com.pl>
2020-12-04 14:13:00 +0100

    Poprawienie klas tworzących trzon  programu
    Dodano opcje rozszerzone jak będzie czas na więcej dłubania. Na razie skupiamy się na zrobieniu i przetestowaniu opcji podstawowej, bo jest uber mało czasu..

--- crop_rotator/rotator/models.py
+++ crop_rotator/rotator/models.py
@@ -31,18 +31,37 @@ class Crop(models.Model):
         (BRASSICACEAE, "Kapustowate"),
         (POACEAE, "Wiechlinowate"),
         (POACEAE_SEASONAL, "Wiechlinowate Jednoroczne"),
-        (OTHER, "każdy"),
+        (OTHER, "Inne"),
         (FABACEAE, "Bobowate"),
         (FABACEAE_PERSISTENT, "Bobowate Wieloletnie"),
         )
+    AS_FAMILY = 0
+    BETTERS = 1
+    WORSENS = 2
+    NEUTRAL = 3
+    AGRICULTURE_STATUS = (
+    (AS_FAMILY, "Domyślnie dla Rodziny"),
+    (BETTERS, "Poprawia Jakość Gleby"),
+    (WORSENS, "Pogarsza Jakość Gleby"),
+    (NEUTRAL, "Neutralna Dla Jakości Gleby"),
+    )
     name = models.CharField(max_length=150)
     descr = models.CharField(max_length=500, blank=True, null=True)
     image = models.ImageField(upload_to='images', blank=True, null=True)
     owner = models.ForeignKey(
-     User, on_delete=models.CASCADE, blank=True, null=True)
+     User, on_delete=models.SET_NULL, blank=True, null=True)
     pubdate = models.DateTimeField(blank=True, null=True)  # Data publikacji
     bio_type = models.PositiveSmallIntegerField(
             choices=BOTANIC_FAMILY, default=3)
+    culture_override = models.PositiveSmallIntegerField(
+                choices=AGRICULTURE_STATUS, default=0)
+    #
+    #
+    #
+    #
+    # Poniższe jest opcjonalne imho. Dodać w rozszerzeniu jak będzie czas.
+    is_summercrop = models.BooleanField(default=False)
+    # Czy jest rośliną jarą niszczoną przez przymrozek?
 
     class Meta:
         ordering = ['-name']
@@ -50,41 +69,58 @@ class Crop(models.Model):
     def __str__(self):
         return self.title
 
-    def pubdate_short(self):
-        return self.pubdate.strftime('%a %d %b %Y')
 
-
-# Mieszanka na miedzplon
+# Mieszanka na miedzyplon
 class CropMix(models.Model):
     name = models.CharField(max_length=150)
     descr = models.CharField(max_length=500, blank=True, null=True)
     image = models.ImageField(upload_to='images', blank=True, null=True)
     owner = models.ForeignKey(
-     User, on_delete=models.CASCADE, blank=True, null=True)
+     User, on_delete=models.SET_NULL, blank=True, null=True)
     pubdate = models.DateTimeField(blank=True, null=True)  # Data publikacji
-    # W tym miejscu trzeba machnąć jakąś listę,
-    # która będzie się odnosić do już istniejących klas "Crop".
+    ingredients = models.ManyToManyField('Crop', blank=True, null=True)
+
+    class Meta:
+        ordering = ['-name']
+
+    def __str__(self):
+        return self.title
 
 
 # Element płodozmianu.
 class RotationStep(models.Model):
     title = models.CharField(max_length=150)
     descr = models.CharField(max_length=500, blank=True, null=True)
+    add_manure = models.BooleanField(default=False)  # Czy dodać nawóz?
     from_plan = models.ForeignKey(
      'RotationPlan', on_delete=models.CASCADE, blank=True, null=True)
     order = models.IntegerField(blank=True, null=True)
     # auto: kolejność w planie.
     crop = models.ForeignKey(
-     'Crop', on_delete=models.CASCADE, blank=True, null=True)
+     'Crop', on_delete=models.SET_NULL, blank=True, null=True)
     # Z listy: plon główny
     co_crop = models.ForeignKey(
-     'Crop', on_delete=models.CASCADE, blank=True, null=True)
+     'Crop', on_delete=models.SET_NULL, blank=True, null=True)
     # Z listy: Wsiewka / uprawa współrzędna
     after_crop = models.ForeignKey(
-     'Crop', on_delete=models.CASCADE, blank=True, null=True)
+     'Crop', on_delete=models.SET_NULL, blank=True, null=True)
     # Międzyplon typu "poplon"
     after_crop_mix = models.ForeignKey(
-     'CropMix', on_delete=models.CASCADE, blank=True, null=True)
+     'CropMix', on_delete=models.SET_NULL, blank=True, null=True)
+    #
+    #
+    #
+    #
+    # Poniższe jest opcjonalne imho. Dodać w rozszerzeniu jak będzie czas.
+    is_ac_main = models.BooleanField(default=False)
+    # Czy poplon jest rośliną ozimą z plonu głównego, którą zamierzamy zebrać
+    # w przyszłym roku?
+    #(Domyślnie fałsz, pojawia się tylko jeśli is_summercrop = False)
+    is_ac_harvest = models.BooleanField(default=False)
+    # Czy zabieramy poplon z pola, czy go zostawiamy na zielony nawóz?
+    # Pojawia się tylko wtedy jeśli roślina ma szanse przetrwać przymrozek
+
+
 
     class Meta:
         ordering = ['-from_plan', 'order']
