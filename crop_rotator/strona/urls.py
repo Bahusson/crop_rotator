c5b62e63c5e18a3a119f2d5e43e7d979d0b1d0cf
Jakub <jakub.kozdrowicz@gmail.com>
2021-01-21 11:13:31 +0100

    Przekierowania działają prawidłowo

--- crop_rotator/strona/urls.py
+++ crop_rotator/strona/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path("crop/<int:crop_id>/", views.crop, name="crop"),
     path("family/<int:family_id>/", views.family, name="family"),
     path("my_plans", views.my_plans, name="my_plans"),
+    path("plan_edit/<int:plan_id>", views.plan_edit, name="plan_edit"),
+    path("step1/edit/<int:plan_id>", views.step1, name="step1"),
 ]
