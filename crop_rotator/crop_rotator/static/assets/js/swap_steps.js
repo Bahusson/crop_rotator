4eb405064cba586152ca3529822b7ae532e9c971
Jakub <jakub.kozdrowicz@gmail.com>
2021-01-29 12:06:37 +0100

    checkpoint

--- crop_rotator/crop_rotator/static/assets/js/swap_steps.js
+++ crop_rotator/crop_rotator/static/assets/js/swap_steps.js
@@ -5,12 +5,18 @@ function hidden_input_value_set()
   document.getElementById("id_gender").value = x;
 }
 
+function hidden_input_name_set()
+{
+  var x = document.getElementById("Genderselector").name;
+  document.getElementById("id_gender").name = x;
+}
+
 $(document).ready(function()
 {
   $("button[name='move_plan']").click(function()
  {
   var button_triger2 = $(event.target);
-  var closest = button_triger2.closest(".p").nextAll(".selection").first().filter(":selected"); // .selected()?
+  var closest = button_triger2.closest(".p").nextAll("select[name='step_selection']").first().filter(":selected"); // .selected()?
   hidden_input_value_set(closest)
   });
 });
