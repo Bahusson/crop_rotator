8de3f8f4016f7859197c377acb259172014fedd7
Jakub <jakub.kozdrowicz@gmail.com>
2021-01-29 13:09:35 +0100

    checkpoint

--- crop_rotator/crop_rotator/static/assets/js/swap_steps.js
+++ crop_rotator/crop_rotator/static/assets/js/swap_steps.js
@@ -1,5 +1,5 @@
 // Prosta funkcja przerzucająca wartość listy rozwijanej do ukrytego formularza.
-function hidden_input_value_set()
+/*function hidden_input_value_set()
 {
   var x = document.getElementById("Genderselector").value;
   document.getElementById("id_gender").value = x;
@@ -9,14 +9,15 @@ function hidden_input_name_set()
 {
   var x = document.getElementById("Genderselector").name;
   document.getElementById("id_gender").name = x;
-}
+}*/
 
 $(document).ready(function()
 {
   $("button[name='move_plan']").click(function()
  {
-  var button_triger2 = $(event.target);
-  var closest = button_triger2.closest(".p").nextAll("select[name='step_selection']").first().filter(":selected"); // .selected()?
-  hidden_input_value_set(closest)
+  alert("button działa!")
+  //var button_triger2 = $(event.target);
+  //var closest = button_triger2.closest(".p").nextAll("select[name='step_selection']").first().filter(":selected"); // .selected()?
+  //hidden_input_value_set(closest)
   });
 });
