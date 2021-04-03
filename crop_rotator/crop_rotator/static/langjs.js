b9067e1cd64dd4a7bcad70986a36746435a0ec21
Jakub <jakub.kozdrowicz@gmail.com>
2021-04-03 17:19:52 +0200

    lang.js domain bump

--- crop_rotator/crop_rotator/static/langjs.js
+++ crop_rotator/crop_rotator/static/langjs.js
@@ -4,11 +4,13 @@ Jak będziesz miał czas to to skróć i zrób tak,
 żeby nie trzeba się grzebać w kodzie,
 jak się ustawi nowy język.*/
 
-var domain = "134.209.227.34" // Tu zmień domenę z localhost albo podepnij to jakoś lepiej.
+var domain = "plodozmian.agro.pl" // Tu zmień domenę z localhost albo podepnij to jakoś lepiej.
+var domain2 = "www.plodozmian.agro.pl"
 
 function setCookie(cvalue)
 {
   document.cookie = "rotator_language = " + cvalue + ";domain=" + domain + ";path=/";
+  document.cookie = "rotator_language = " + cvalue + ";domain=" + domain2 + ";path=/";
   location.reload();
 }
 
